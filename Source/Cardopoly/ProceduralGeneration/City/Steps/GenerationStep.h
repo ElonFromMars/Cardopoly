cb05940e4790cf2e18ae47851b5125e6d3713f86
Daniel Shulgin <danielshulgin37@gmail.com>
2024-12-18 23:47:00 +0100

    add constructions procedural generation(in progress)

---
+++ Source/Cardopoly/ProceduralGeneration/City/Steps/GenerationStep.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "Cardopoly/ProceduralGeneration/City/CityGenerationData.h"
+
+class GenerationStep
+{
+public:
+	GenerationStep(CityGenerationData& cityGenerationData)
+		: _cityGenerationData(cityGenerationData)
+	{
+		
+	}
+	
+protected:
+	CityGenerationData& _cityGenerationData;
+};
--- Source/Cardopoly/ProceduralGeneration/City/Steps/LSystemConstructionsGenerationStep.h
+++ Source/Cardopoly/ProceduralGeneration/City/Steps/LSystemConstructionsGenerationStep.h
@@ -1,7 +1,17 @@
 ﻿#pragma once
 
-class LSystemConstructionsGenerationStep
+#include "GenerationStep.h"
+
+class LSystemConstructionsGenerationStep : public GenerationStep
 {
 public:
+	LSystemConstructionsGenerationStep(CityGenerationData& cityGenerationData)
+		: GenerationStep(cityGenerationData)
+	{ }
 	
+	void Initialize();
+
+	void SubStep();
+
+	bool IsFinished() const;
 };
